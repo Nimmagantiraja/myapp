2073fb05f35a3e82d01562b4bb2123b73572f1cc
Raja.N <Raja.N@LAPTOP-PEABOB1V>
2019-05-09 15:03:43 +0530

    Ticket-4536 new project created

---
+++ src/myapp/Employee.java
@@ -0,0 +1,22 @@
+package myapp;
+
+public class Employee {
+	private int empId;
+	private String empName;
+	private double empSal;
+	public Employee() {
+		super();
+	}
+	public Employee(int empId, String empName, double empSal) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSal = empSal;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
+	}
+	
+
+}
